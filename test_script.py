c1d2dbb78b9b4950271b0570cddfc2d119b27faa
beingkk <beingkk@users.noreply.github.com>
2019-04-01 16:55:09 +0200

    test_script.py

--- test_script.py
+++ test_script.py
@@ -6,8 +6,8 @@
 import config
 import tweepy
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
 
 api = tweepy.API(auth)
 
