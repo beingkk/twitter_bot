cb2d169daa09e039692c656f0233e7187d08b137
beingkk <beingkk@users.noreply.github.com>
2019-04-01 16:50:55 +0200

    test_script.py

---
+++ test_script.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+import config
+import tweepy
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text)
