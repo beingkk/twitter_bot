be5159feb1690c4a56daa6bdb19ad391590cf2a2
laurensMac <laurens@Laurenss-MacBook-Pro.local>
2019-05-23 17:20:44 +0200

    Added error message and truncated tweet handling

--- check_sentiment.py
+++ check_sentiment.py
@@ -7,10 +7,20 @@ auth.set_access_token(config.access_token, config.access_token_secret)
 api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
+    def on_error(self, status_code):
+        if status_code == 420:
+            print('ERROR 420')
+            #returning False in on_error disconnects the stream
+            return False
     def on_status(self, status):
-        print(status.text)
+        if status.truncated:
+            print(status.extended_tweet['full_text'])
+        else:
+            print(status.text)
+
+        print("--------------------------------------")
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 
-myStream.filter(track=['bitcoin'])
+myStream.filter(track=['trump'])
